8e6ab4210eecf7de9ec566ba0fa575d12f89d701
2bfv5gtjth-droid <2bfv5gtjth@privaterelay.appleid.com>
2026-09-22 00:00:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='logostudio-v6';
+const V='logostudio-v71';
 const CORE=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V&&k.startsWith('logostudio-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
@@ -7,7 +7,7 @@ self.addEventListener('fetch',e=>{
   const u=new URL(r.url),fonts=/^fonts\.(googleapis|gstatic)\.com$/.test(u.hostname);
   if(u.origin!==location.origin&&!fonts) return;
   if(r.mode==='navigate'){
-    e.respondWith(fetch(r).then(res=>{const cp=res.clone();caches.open(V).then(c=>c.put('index.html',cp));return res;}).catch(()=>caches.match('index.html')));
+    e.respondWith(fetch(r,{cache:'reload'}).then(res=>{const cp=res.clone();caches.open(V).then(c=>c.put('index.html',cp));return res;}).catch(()=>caches.match('index.html')));
     return;
   }
   e.respondWith(caches.match(r).then(hit=>{
